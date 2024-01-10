651fc4d3cd6a2f97ee9b2f6aee0a49357d181c8a
Dylanos03 <dylan2.osullivan@live.uwe.ac.uk>
2024-01-10 18:24:37 +0000

    commented out unnecessary trpc code

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -11,18 +11,18 @@ export const postRouter = createTRPCRouter({
       };
     }),
 
-  create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+  // create: publicProcedure
+  //   .input(z.object({ name: z.string().min(1) }))
+  //   .mutation(async ({ ctx, input }) => {
+  //     // simulate a slow db call
+  //     await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-        },
-      });
-    }),
+  //     return ctx.db.post.create({
+  //       data: {
+  //         name: input.name,
+  //       },
+  //     });
+  //   }),
 
   getLatest: publicProcedure.query(({ ctx }) => {
     return ctx.db.post.findFirst({
