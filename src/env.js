1f6b95c58a94af09077c89a0f7db386833f1a0e4
Dylanos03 <dylan2.osullivan@live.uwe.ac.uk>
2024-01-10 17:00:16 +0000

    final env?

--- src/env.js
+++ src/env.js
@@ -10,10 +10,7 @@ export const env = createEnv({
     DATABASE_URL: z
       .string()
       .url()
-      .refine(
-        (str) => !str.includes("@aws.connect.psdb.cloud/homeys"),
-        "You forgot to change the default URL",
-      ),
+      .refine((str) => !str.includes("@aws.connect.psdb.cloud/homeys")),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -27,6 +24,7 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
   },
 
   /**
@@ -37,6 +35,8 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
