5091f6d71a4b7899cad984a65b39b42585166a3e
Dylanos03 <dylan2.osullivan@live.uwe.ac.uk>
2024-01-10 16:53:46 +0000

    env sorted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default async function Home() {
         </div>
 
         <CrudShowcase />
-        <UserButton afterSignOutAllUrl="/" />
+        <UserButton afterSignOutUrl="/" />
       </div>
     </main>
   );
