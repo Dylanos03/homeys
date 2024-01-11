0db4a0f6e24867264080bef2aaa5bf4c3b6eea7a
Dylanos03 <dylan2.osullivan@live.uwe.ac.uk>
2024-01-11 13:03:29 +0000

    moved time since function to a Utils folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { api } from "~/trpc/server";
 import Sidebar from "./_components/sidebar";
 import Link from "next/link";
+import { timeSince } from "~/utils/timeSince";
 
 type Post = {
   id: number;
@@ -10,28 +11,6 @@ type Post = {
   userId: string;
   createdAt: Date;
 };
-
-function timeSince(date: Date) {
-  let overHour = false;
-  let overDay = false;
-  let diffHr = 0;
-  let diffDay = 0;
-
-  const dateNow = new Date();
-  const diff = dateNow.getTime() - date.getTime();
-  const diffMin = Math.round(diff / 60000);
-  if (diffMin >= 60) {
-    diffHr = Math.round(diffMin / 60);
-    overHour = true;
-    return diffHr + ` hour${diffHr! > 1 ? "'s" : ""} ago`;
-  }
-  if (diffHr >= 24) {
-    overDay = true;
-    diffDay = Math.round(diffHr / 24);
-    return diffDay + ` day${diffHr! > 1 ? "'s" : ""} ago`;
-  }
-  return diffMin + " mins ago";
-}
 
 const CreatePostButton = () => {
   return (
