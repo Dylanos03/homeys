2d107e7c93b61dcfaf4f10619976e3059bf33a9a
Dylanos03 <dylan2.osullivan@live.uwe.ac.uk>
2024-01-11 19:46:20 +0000

    import typo

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -2,7 +2,7 @@
 import { UserButton, useUser, SignInButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
-import logo from "../../../public/logo.webp";
+import logo from "../../../public/Logo.webp";
 
 function Navbar() {
   const { isSignedIn } = useUser();
