72a629c41f027ba08259ebc4cd3fcf9ff9abc782
Dylanos03 <dylan2.osullivan@live.uwe.ac.uk>
2024-01-17 15:27:06 +0000

    cross origin error

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <SignIn redirectUrl={"/"} />
+      <SignIn afterSignInUrl={"/"} />
     </div>
   );
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 function SignUpPage() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <SignUp redirectUrl={"/users/create-profile"} />
+      <SignUp afterSignUpUrl={"/users/create-profile"} />
     </div>
   );
 }
