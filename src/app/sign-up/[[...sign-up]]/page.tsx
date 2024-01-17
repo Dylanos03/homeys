518d098a851adf849ab033ed886d867fe27a798f
Dylanos03 <dylan2.osullivan@live.uwe.ac.uk>
2024-01-17 15:32:49 +0000

    new links

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 function SignUpPage() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <SignUp afterSignUpUrl={"/users/create-profile"} />
+      <SignUp afterSignUpUrl={"/users/create-profile"} signInUrl="/sign-in" />
     </div>
   );
 }
