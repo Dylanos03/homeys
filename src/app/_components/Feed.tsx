7e33f14d93215326904d892da5e80e86a765d37a
Dylanos03 <dylan2.osullivan@live.uwe.ac.uk>
2024-01-31 12:10:06 +0000

    feed moved to seperate file so it can use client, home still ssr, filtering of feed at the top

--- src/app/_components/Feed.tsx
+++ src/app/_components/Feed.tsx
@@ -1,5 +1,95 @@
+"use client";
+
+import PostCard from "./PostCard";
+import { CreatePost } from "./PostCreator";
+import { api } from "~/trpc/react";
+import LoadingSpinner from "./loadingSpinner";
+import { useUser } from "@clerk/nextjs";
+import { useState } from "react";
+
 function Feed() {
-  return <></>;
+  const { data, isLoading } = api.post.getAll.useQuery();
+  const { user } = useUser();
+  const [byLocation, setByLocation] = useState(false);
+  const [byUniversity, setByUniversity] = useState(false);
+
+  const filteredBothData = data
+    ?.filter(
+      (post) =>
+        post.userUniversity === user?.unsafeMetadata.userUniversity &&
+        post.userLocation === user?.unsafeMetadata.userLocation,
+    )
+    .map((post) => <PostCard {...post} key={post.id} />);
+
+  const filteredUniData = data
+    ?.filter(
+      (post) => post.userUniversity === user?.unsafeMetadata.userUniversity,
+    )
+    .map((post) => <PostCard {...post} key={post.id} />);
+
+  const filteredLocData = data
+    ?.filter((post) => post.userLocation === user?.unsafeMetadata.userLocation)
+    .map((post) => <PostCard {...post} key={post.id} />);
+
+  const noFilterData = data?.map((post) => (
+    <PostCard {...post} key={post.id} />
+  ));
+
+  return (
+    <>
+      {" "}
+      <div className="flex w-[720px] flex-col border-x-2">
+        {user && (
+          <div className="gap sticky top-0 flex w-full justify-between border-b-2  bg-brandLight  font-semibold">
+            <button
+              onClick={() => setByLocation(!byLocation)}
+              className={`flex w-1/2 justify-center border-r-2 py-2 ${
+                byLocation && "bg-brandOrange text-white"
+              }`}
+            >
+              Your Location
+            </button>
+            <button
+              onClick={() => setByUniversity(!byUniversity)}
+              className={`flex w-1/2 justify-center py-2 ${
+                byUniversity && "bg-brandOrange text-white"
+              }`}
+            >
+              Your University
+            </button>
+          </div>
+        )}
+        {user && <CreatePost />}
+        {(() => {
+          switch (true) {
+            case byLocation && byUniversity:
+              return filteredBothData;
+            case byUniversity && !byLocation:
+              return filteredUniData;
+            case byLocation && !byUniversity:
+              return filteredLocData;
+            case !byLocation && !byUniversity:
+              return noFilterData;
+            default:
+              return noFilterData;
+          }
+        })()}
+        {data?.length === 0 && (
+          <div className="mt-8 flex w-full flex-col items-center gap-3 border-b-2 border-slate-100 p-4 px-4   text-brandDark">
+            <h3>It&apos;s pretty quiet in here...</h3>
+            <h4 className="text-2xl font-bold">
+              Create a post and get the conversations started
+            </h4>
+          </div>
+        )}
+        {isLoading && (
+          <div className="flex justify-center">
+            <LoadingSpinner />
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default Feed;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import { api } from "~/trpc/server";
 import Sidebar from "./_components/sidebar";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFilter, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
-import PostCard from "./_components/PostCard";
-import { CreatePost } from "./_components/PostCreator";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import Feed from "./_components/Feed";
 
 export type Post = {
   id: number;
@@ -32,44 +30,11 @@ const CreatePostButton = () => {
 };
 
 export default async function Home() {
-  const data = await api.post.getAll.query();
-
   return (
     <main className="relative m-0 flex min-h-screen w-screen justify-center  px-4 ">
       <Sidebar />
       <CreatePostButton />
-      <div className="flex w-[720px] flex-col border-x-2">
-        <div className="sticky top-0 flex w-full justify-between gap-3 border-b-2 border-slate-100 bg-brandLight px-4 font-semibold">
-          <Link
-            className="flex w-1/2 justify-center border-r-2 py-2 "
-            href={"/"}
-          >
-            People
-          </Link>
-          <Link
-            className="flex w-1/2 justify-center border-r-2 py-2"
-            href={"/"}
-          >
-            Houses
-          </Link>
-          <button>
-            <FontAwesomeIcon icon={faFilter} style={{ color: "#bd5103" }} />
-          </button>
-        </div>
-        <CreatePost />
-        {data.length === 0 && (
-          <div className="mt-8 flex w-full flex-col items-center gap-3 border-b-2 border-slate-100 p-4 px-4   text-brandDark">
-            <h3>It&apos;s pretty quiet in here...</h3>
-            <h4 className="text-2xl font-bold">
-              Create a post and get the conversations started
-            </h4>
-          </div>
-        )}
-
-        {data.map((post) => (
-          <PostCard {...post} key={post.id} />
-        ))}
-      </div>
+      <Feed />
     </main>
   );
 }
